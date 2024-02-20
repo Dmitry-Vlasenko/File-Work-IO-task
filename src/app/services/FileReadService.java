4ec8453517c607837d5d982492b7ce0ba412ff77
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-02-20 23:54:18 +0200

    add read view

---
+++ src/app/services/FileReadService.java
@@ -0,0 +1,28 @@
+package app.services;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class FileReadService {
+    public static String readFile(String path) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader currentFileBufferRider = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(path)
+                )
+        )
+        ) {
+            String line;
+            while ((line = currentFileBufferRider.readLine()) != null) {
+                content.append(line);
+                content.append("\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Problem with read file");
+        }
+        return content.toString();
+    }
+}
